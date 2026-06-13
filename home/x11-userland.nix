7b7fdace544e4a7b728baf35e48146e2bda25a14
Cameron Candau <121522527+CameronCandau@users.noreply.github.com>
2026-06-13 13:13:38 -0700

    Change x11 terminal to xfce4-terminal

--- home/x11-userland.nix
+++ home/x11-userland.nix
@@ -1,15 +1,17 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    alacritty
     curl
     fd
     git
     jq
     ripgrep
     unzip
+    xfce.xfce4-terminal
     xclip
     xsel
     zip
   ];
+
+  home.file.".config/xfce4/terminal/terminalrc".source = ../files/xfce4-terminal/terminalrc;
 }
